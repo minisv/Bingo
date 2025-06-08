6a6ec419b7b33b7010338bdf3e1b79df62cb65d3
minisv <minisv@naver.com>
2025-06-08 15:57:06 +0900

    Init: init

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/bingo/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Bingo/' : '/',
 })
